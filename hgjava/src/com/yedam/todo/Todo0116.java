706501bfb1b1bd37b2e8596f245c430e6bf36abd
qjdxj <qjdxjd0643@DESKTOP-QGE1P3P>
2024-01-16 23:53:54 +0900

    과제1

--- hgjava/src/com/yedam/todo/Todo0116.java
+++ hgjava/src/com/yedam/todo/Todo0116.java
@@ -2,19 +2,39 @@ package com.yedam.todo;
 
 import java.util.Scanner;
 
-public class Todo0116 {
+	public class Todo0116 {
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		
-		System.out.println("값을 입력>>>");
+		Scanner scn = new Scanner(System.in); //System.in <= 키보드입력값
 		String input = scn.nextLine(); 
-		// input.equals("quit")
+		
+		input.equals("quit");
+		
+//		for(int a=1; a<10; a++) {
+//			if (input == "quit") {
+//				
+//			}
+//		}
+//		
+//			
+//		}
+//		while(true) {			
+//			if(input == "quit") {
+//				System.out.println("<<end of prog>>");
+//				break;
+//			}else System.out.println("값을 입력>>>");
+//			
+//		} 
+		
+		
+		
+	
+		
+		
 		
 		
 		// 입력값은 홍길동 입니다.
 		// input 이랑 quit 값 비교
 		// "quit" 값이 들어오면 <<end of prog>>
 		
-		//
 	}
 }
